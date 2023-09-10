7398d01d842fac2ae153e737ae29d43458cbce60
bradsway <48184644+bradsway@users.noreply.github.com>
2023-09-10 16:29:59 -0700

    Animations
    Goblin and gems animations

--- Entity.js
+++ Entity.js
@@ -1,16 +1,28 @@
 class Entity {
     states = {
-        IDLE: 0,
+        IDLE : 0,
         SEARCHING : 1,
         CHASING : 2
     };
-    // searchingWaypoints = [{x:38,y:1},{x:4,y:3},{x:1,y:27}];
+    directions = {
+        DOWN : 0,
+        LEFT : 1,
+        RIGHT: 2,
+        UP : 3
+    };
+    // ANIMATION VARIABLES
+    facing = this.directions.UP;
+    frame = 0;
+    elapsedTime = 0;
+    frameChangeInterval = 0.1;//IN SECONDS
+    //WAY POINTS FOR SEARCHING
     searchingWaypoints = [{x:6,y:1},{x:4,y:3},{x:3,y:6}];
     target = null;
     waypointPointer = null;
     tweenPercent = 0;
     path = [];
     state = this.states.CHASING;
+    //OTHER
     spriteSheet = {sprite:null,fileName:'',spriteSize:{width:0,height:0},cellSize:{width:0,height:0},grid:{rows:0,columns:0}};
     position = {x:0, y:0};
     direction = "none";
@@ -30,13 +42,14 @@ class Entity {
     }
     brain = (deltaTime) => {
         let distance = Math.sqrt(Math.pow((this.position.x-this.gameController.player.position.x),2) + Math.pow((this.position.y-this.gameController.player.position.y),2));
-        if(distance < 150) {
+        if(distance < 50) {
             if(this.state = this.states.SEARCHING){this.target = null;this.path = [];}
             this.state = this.states.CHASING;
         }
         else {
             this.state = this.states.SEARCHING;
         }
+        let result2 = null;
         switch (this.state) {
             case this.states.IDLE:
                 console.log("Idle state");
@@ -48,7 +61,7 @@ class Entity {
                     this.findWaypoint();
                 }
                
-                let result2 = Tools2D.moveTowards_CloseEnough(deltaTime,this.position,this.path[1],this.speed-10,8);//move to the next waypoint
+                result2 = Tools2D.moveTowards_CloseEnough(deltaTime,this.position,this.path[1],this.speed-10,8);//move to the next waypoint
                 if(result2!=null){
                     if(result2.hit){                    
                         this.path.shift();
@@ -67,29 +80,45 @@ class Entity {
                     this.path = this.gameController.levelMap.aStar.findPath().map(pos=>{return {x:(pos.x*32)+16,y:(pos.y*32)+16}}).reverse();
                 }
                 if(this.path.length === 1) {
+                    this.frame=0;
                     break;
                 }
-                //console.log(this.path);
-                let result3 = Tools2D.moveTowards_CloseEnough(deltaTime,this.position,this.path[1],this.speed-5,8);//move to the next waypoint
-                if(result3.hit){                    
+                result2 = Tools2D.moveTowards_CloseEnough(deltaTime,this.position,this.path[1],this.speed-5,8);//move to the next waypoint
+                if(result2.hit){                    
                     this.path.shift();
                 }
                 break;
             default:
               console.log("Yep, no state");
           }
+          if(result2 != null){
+            let angle = result2.angle * (180 / Math.PI);
+            if(Math.abs(angle) <= 180 && Math.abs(angle) > 135) {this.facing = this.directions.LEFT;}
+            if(Math.abs(angle) <= 45 && Math.abs(angle) > 0) {this.facing = this.directions.RIGHT;}
+            if(angle >= 0) {if(Math.abs(angle) <= 135 && Math.abs(angle) > 45) {this.facing = this.directions.DOWN;}}
+            else {if(Math.abs(angle) <= 135 && Math.abs(angle) > 45) {this.facing = this.directions.UP;}}
+          }
+            
+    }
+    nextFrame = (deltaTime) => {
+        this.elapsedTime += deltaTime;
+
+        if (this.elapsedTime >= this.frameChangeInterval) {
+            this.frame += 1;
+            this.frame %= 4;
+            this.elapsedTime = 0; // Reset the elapsed time
+        }
     }
     update = (deltaTime) => {
+        this.nextFrame(deltaTime);
         this.brain(deltaTime);
-        drawImageSprite(this.gameController.currentScene.backBuffer, this.spriteSheet.sprite,0,0,
-                this.spriteSheet.cellSize.width,
-                this.spriteSheet.cellSize.height,
-                this.position.x,
-                this.position.y,
-                this.spriteSheet.spriteSize.width,
-                this.spriteSheet.spriteSize.height            
-            );
-
-
+        drawImageSprite(this.gameController.currentScene.backBuffer, this.spriteSheet.sprite, 1 + (11 * this.frame), 1 + (18 * this.facing),
+            this.spriteSheet.cellSize.width,
+            this.spriteSheet.cellSize.height,
+            this.position.x,
+            this.position.y,
+            this.spriteSheet.spriteSize.width,
+            this.spriteSheet.spriteSize.height
+        );
     }
 }
--- GameController.js
+++ GameController.js
@@ -1,4 +1,15 @@
 class GameController {
+    gemTypes = {
+        DIAMOND : 0,
+        RUBY : 1,
+        EMERALD : 2
+    }
+    //ANIMATION
+    frame = 0;
+    elapsedTime = 0;
+    frameChangeInterval = 0.1;//IN SECONDS
+    //OTHER
+    gems = [{type:this.gemTypes.DIAMOND,pos:{x:64,y:64}},{type:this.gemTypes.EMERALD,pos:{x:96,y:64}},{type:this.gemTypes.RUBY,pos:{x:128,y:64}}];
     images = {};
     sounds = {};
     entities = [];
@@ -15,7 +26,7 @@ class GameController {
         this.currentScene = scene;
         this.camera = scene.camera;
         this.levelMap = new LevelMap(this);
-        loadImages(["Images/player.png","Images/map1.png","Images/player_testing.png","Images/lightsource.png"],this.imagesFinished);
+        loadImages(["Images/goblin_sheet.png","Images/map1.png","Images/player_testing.png","Images/lightsource.png","Images/gems.png"],this.imagesFinished);
         loadSounds(["Sounds/noise.wav"],this.soundsFinished);        
     }
 
@@ -38,9 +49,9 @@ class GameController {
     finishedLoading = () => {
         let e = new Entity(this,{
             sprite:null, 
-            fileName:"Images/player.png",
-            cellSize:{width:16,height:16},
-            spriteSize:{width:16,height:16},
+            fileName:"Images/goblin_sheet.png",
+            cellSize:{width:10,height:17},
+            spriteSize:{width:10,height:17},
             grid:{rows:4,columns:4}
         }, {x:(32*2)+16,y:(32*2)+16});
         this.entities.push(e);
@@ -51,8 +62,17 @@ class GameController {
         this.player = new Player(this);
         this.resouncesReady = true;
     }
+    nextFrame = (deltaTime) => {
+        this.elapsedTime += deltaTime;
 
+        if (this.elapsedTime >= this.frameChangeInterval) {
+            this.frame += 1;
+            this.frame %= 6;
+            this.elapsedTime = 0; // Reset the elapsed time
+        }
+    }
     update = (deltaTime) => {
+        this.nextFrame(deltaTime);
         if(!this.resouncesReady) return;
 
         drawImageFrom00(this.currentScene.backBuffer, this.images["Images/map1.png"],
@@ -63,6 +83,10 @@ class GameController {
         this.entities.forEach(entity => {
             entity.update(deltaTime);
         });
+        for (let i = 0; i < this.gems.length; i ++) {
+            drawImageSprite(this.currentScene.backBuffer, this.images["Images/gems.png"], this.frame*10, this.gems[i].type*10, 9, 9, this.gems[i].pos.x, this.gems[i].pos.y, 10, 10);
+            if(Collision.testCircleOnCircle(this.player.position,8,this.gems[i].pos,8))this.gems.splice(i, 1);
+        }
         this.player.update(deltaTime);
     }
 }
